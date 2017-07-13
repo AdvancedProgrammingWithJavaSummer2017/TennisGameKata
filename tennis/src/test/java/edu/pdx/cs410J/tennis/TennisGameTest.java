dad0bc011234fe92e6a921d4cbfe0ecc44cc490e
David Whitlock <david.m.whitlock@gmail.com>
2017-07-12 20:05:16 -0700

    Get rid of the usages of the flight stuff.

--- tennis/src/test/java/edu/pdx/cs410J/tennis/TennisGameTest.java
+++ tennis/src/test/java/edu/pdx/cs410J/tennis/TennisGameTest.java
@@ -1,31 +1,9 @@
 package edu.pdx.cs410J.tennis;
 
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 /**
  * Unit tests for the {@link TennisGame} class.
  */
 public class TennisGameTest {
   
-  @Test(expected = UnsupportedOperationException.class)
-  public void getArrivalStringNeedsToBeImplemented() {
-    TennisGame flight = new TennisGame();
-    flight.getArrivalString();
-  }
 
-  @Test
-  public void initiallyAllFlightsHaveTheSameNumber() {
-    TennisGame flight = new TennisGame();
-    assertThat(flight.getNumber(), equalTo(42));
-  }
-
-  @Test
-  public void forProject1ItIsOkayIfGetDepartureTimeReturnsNull() {
-    TennisGame flight = new TennisGame();
-    assertThat(flight.getDeparture(), is(nullValue()));
-  }
-  
 }
