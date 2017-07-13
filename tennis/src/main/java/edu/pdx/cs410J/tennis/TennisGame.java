ca9fd334a1826c06bb3b282d8424270160c5eed1
David Whitlock <david.m.whitlock@gmail.com>
2017-07-12 21:09:12 -0700

    David attempts to implement the kata.

--- tennis/src/main/java/edu/pdx/cs410J/tennis/TennisGame.java
+++ tennis/src/main/java/edu/pdx/cs410J/tennis/TennisGame.java
@@ -2,4 +2,62 @@ package edu.pdx.cs410J.tennis;
 
 public class TennisGame {
 
+  private final Player player1 = new Player();
+  private final Player player2 = new Player();
+
+  public int getPlayer1Score() {
+    return this.player1.getScore();
+  }
+
+  public int getPlayer2Score() {
+    return this.player2.getScore();
+  }
+
+  void player1WinsVolley() {
+    this.player1.winVolley(this.player2.getScore());
+  }
+
+  void player2WinsVolley() {
+    this.player2.winVolley(this.player1.getScore());
+  }
+
+  public boolean player1IsWinner() {
+    return this.player1.isWinner();
+  }
+
+  private static class Player {
+
+    private int score = 0;
+    private boolean isWinner = false;
+
+    int getScore() {
+      return score;
+    }
+
+    void winVolley(int opponentScore) {
+      this.score = nextScore(this.score, opponentScore);
+    }
+
+    private int nextScore(int currentScore, int opponentScore) {
+      switch (currentScore) {
+        case 0:
+          return 15;
+        case 15:
+          return 30;
+        case 30:
+          return 40;
+        case 40:
+          if (opponentScore != 40) {
+            this.isWinner = true;
+          }
+          return 40;
+        default:
+          throw new UnsupportedOperationException("Don't know how to get score after " + currentScore);
+      }
+    }
+
+    public boolean isWinner() {
+      return this.isWinner;
+    }
+  }
 }
--- tennis/src/test/java/edu/pdx/cs410J/tennis/TennisGameTest.java
+++ tennis/src/test/java/edu/pdx/cs410J/tennis/TennisGameTest.java
@@ -1,9 +1,83 @@
 package edu.pdx.cs410J.tennis;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * Unit tests for the {@link TennisGame} class.
  */
 public class TennisGameTest {
   
+  @Test
+  public void initiallyBothPlayersHaveAScoreOf0() {
+    TennisGame game = new TennisGame();
+    assertThat(game.getPlayer1Score(), equalTo(0));
+    assertThat(game.getPlayer2Score(), equalTo(0));
+  }
+
+  @Test
+  public void whenPlayer1WinsVolleyPlayer1HasScoreOf15() {
+    TennisGame game = new TennisGame();
+    game.player1WinsVolley();
+    assertThat(game.getPlayer1Score(), equalTo(15));
+    assertThat(game.getPlayer2Score(), equalTo(0));
+  }
+
+  @Test
+  public void whenPlayer2WinsVolleyPlayer2HasScoreOf15() {
+    TennisGame game = new TennisGame();
+    game.player2WinsVolley();
+    assertThat(game.getPlayer1Score(), equalTo(0));
+    assertThat(game.getPlayer2Score(), equalTo(15));
+  }
+
+  @Test
+  public void whenPlayer1WinsTwoVolleysPlayer1HasScoreOf30() {
+    TennisGame game = new TennisGame();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    assertThat(game.getPlayer1Score(), equalTo(30));
+    assertThat(game.getPlayer2Score(), equalTo(0));
+  }
+
+  @Test
+  public void whenPlayer1WinsThreeVolleysPlayer1HasScoreOf40() {
+    TennisGame game = new TennisGame();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    assertThat(game.getPlayer1Score(), equalTo(40));
+    assertThat(game.getPlayer2Score(), equalTo(0));
+  }
+
+  @Test
+  public void whenPlayer1WinsFourVolleysPlayer1HasWonTheGame() {
+    TennisGame game = new TennisGame();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    assertThat(game.player1IsWinner(), equalTo(true));
+  }
+
+  @Test
+  public void player1WinsVolleyWhenScoreIsTiedAt40() {
+    TennisGame game = new TennisGame();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    game.player1WinsVolley();
+    game.player2WinsVolley();
+    game.player2WinsVolley();
+    game.player2WinsVolley();
+    game.player1WinsVolley();
+    assertThat(game.getPlayer1Score(), equalTo(40));
+    assertThat(game.getPlayer2Score(), equalTo(40));
+
+    assertThat(game.player1IsWinner(), equalTo(false));
+    assertThat(game.player1IsWinner(), equalTo(false));
+
+  }
 
 }
